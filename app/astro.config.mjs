ab2aa4cdde44d00f28dce6d19e4fa9637237f35e
Ayane <necromancer@minakami.land>
2023-09-16 22:17:59 +0800

    feat(app): configure sanity

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,8 +1,21 @@
-import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import sanity from '@sanity/astro'
+import { loadEnv } from 'vite'
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [react(), tailwind()]
-});
+export default defineConfig(({ mode }) => {
+  Object.assign(process.env, loadEnv(mode, process.cwd()))
+  return {
+    integrations: [
+      react(),
+      tailwind(),
+      sanity({
+        projectId: process.env.SECRET_PROJECT_ID,
+        dataset: process.env.SECRET_DATASET,
+        useCdn: false,
+      }),
+    ],
+  }
+})
