f078bcf3a40424f83a7b31b13e3ea1bdbc723997
Ayane <necromancer@minakami.land>
2023-09-19 01:31:52 +0800

    feat: env variables for monorepo

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   name: 'default',
   title: '',
 
-  projectId: '',
-  dataset: '',
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID ?? '',
+  dataset: process.env.SANITY_STUDIO_DATASET ?? '',
 
   plugins: [deskTool(), visionTool()],
 
