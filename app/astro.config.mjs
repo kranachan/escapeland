0331bca50fd4d2db1b431bd4dd3d1a511da93307
Ayane <me@ayane.io>
2023-09-18 22:43:46 +0800

    chore: clean up

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -2,18 +2,16 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import sanity from '@sanity/astro'
-import { loadEnv } from 'vite'
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-      react(),
-      tailwind(),
-      sanity({
-        projectId: process.env.SECRET_PROJECT_ID,
-        dataset: process.env.SECRET_DATASET,
-        useCdn: false,
-      }),
-    ],
-  }
-)
+  integrations: [
+    react(),
+    tailwind(),
+    sanity({
+      projectId: process.env.SECRET_PROJECT_ID,
+      dataset: process.env.SECRET_DATASET,
+      useCdn: false,
+    }),
+  ],
+})
--- app/src/env.d.ts
+++ app/src/env.d.ts
@@ -1,10 +1,10 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly SECRET_PROJECT_ID: string;
-  readonly SECRET_DATASET: string;
+  readonly SECRET_PROJECT_ID: string
+  readonly SECRET_DATASET: string
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv;
+  readonly env: ImportMetaEnv
 }
