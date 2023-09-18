e9185b9de6b958b1e19017a5e7e74dc6ba537349
Ayane <necromancer@minakami.land>
2023-09-19 02:07:32 +0800

    feat: remove unnecessary author fields

--- studio/schemas/index.ts
+++ studio/schemas/index.ts
@@ -1,6 +1,5 @@
 import blockContent from './blockContent'
 import category from './category'
 import post from './post'
-import author from './author'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [post, category, blockContent]
