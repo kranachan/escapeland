ddd8abb6ad6f40576cdc70c087585c3cc7969048
Ayane <me@ayane.io>
2023-09-19 02:07:32 +0800

    feat: remove unnecessary author fields

--- studio/schemas/post.ts
+++ studio/schemas/post.ts
@@ -20,12 +20,6 @@ export default defineType({
       },
     }),
     defineField({
-      name: 'author',
-      title: 'Author',
-      type: 'reference',
-      to: { type: 'author' },
-    }),
-    defineField({
       name: 'mainImage',
       title: 'Main image',
       type: 'image',
@@ -54,12 +48,7 @@ export default defineType({
   preview: {
     select: {
       title: 'title',
-      author: 'author.name',
       media: 'mainImage',
-    },
-    prepare(selection) {
-      const { author } = selection
-      return { ...selection, subtitle: author && `by ${author}` }
     },
   },
 })
