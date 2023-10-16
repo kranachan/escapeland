3331bf365b3a6533bdc745335ea6e71a995d2bde
Ayane <necromancer@minakami.land>
2023-10-16 23:39:09 +0800

    chore(app): rename `PostItem` to `PostCard`

--- app/src/components/PostCard.tsx
+++ app/src/components/PostCard.tsx
@@ -1,12 +1,12 @@
-import type { AnchorHTMLAttributes, FC } from 'react'
+import { type AnchorHTMLAttributes, type FC } from 'react'
 import { Categories } from './Categories'
 import type { Post } from '~/sanity/schemas/post'
 
-export interface PostItemProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
+export interface PostCardProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
   preview: Omit<Post, 'mainImage' | 'body'>
 }
 
-export const PostItem: FC<PostItemProps> = (props) => {
+export const PostCard: FC<PostCardProps> = (props) => {
   const { preview, children, ...rest } = props
   return (
     <a className="flex flex-col gap-1" href={`post/${preview.slug}`} {...rest}>
