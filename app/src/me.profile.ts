1cace85f440150aa57b2e5d151ffdc64657e3f7c
Ayane <me@ayane.io>
2023-10-28 21:31:20 +0800

    chore(app): update profiles

--- app/src/me.profile.ts
+++ app/src/me.profile.ts
@@ -1,10 +1,11 @@
 export default {
-  name: 'Ayane',
-  bio: '* Artistic creation of egocentrism, \n  Dream of being a solo-rock engineer.',
+  name: 'Minakami',
   contacts: {
     github: 'https://github.com/miyakoochi',
     telegram: 'https://t.me/miyakoochi',
     'e-mail': 'me@ayane.io',
     // ...and more
   },
+  repository: 'https://github.com/miyakoochi/escapeland',
+  archives: 'https://archives.ayane.io',
 }
