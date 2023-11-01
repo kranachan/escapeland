e5d9047228407416db6410f79990a065eacc4249
Ayane <me@ayane.io>
2023-11-02 03:29:38 +0800

    fix(app): safely fetch profile data

--- app/src/store/profile.ts
+++ app/src/store/profile.ts
@@ -1,5 +1,7 @@
 import { map } from 'nanostores'
 import type { Profile } from '~/sanity/schemas/profile'
+import { runQuery } from '~/sanity/run-query'
+import { profileQuery } from '~/sanity/schemas/profile'
 
 export const $profile = map<{ data: NonNullable<Profile>; isFetched: boolean }>(
   {
@@ -7,3 +9,16 @@ export const $profile = map<{ data: NonNullable<Profile>; isFetched: boolean }>(
     isFetched: false,
   },
 )
+
+export const fetchProfile = async (): Promise<NonNullable<Profile>> => {
+  if (!$profile.get().isFetched) {
+    $profile.setKey('isFetched', true)
+    const response = await runQuery(profileQuery)
+
+    if (response) {
+      $profile.setKey('data', response)
+    }
+  }
+
+  return $profile.get().data
+}
