b17f74e3c25eeb2574c78a966b94d017fb599c60
Minakami <necromancer@minakami.land>
2023-12-06 13:19:46 +0800

    feat(app): define colors

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -7,6 +7,13 @@ module.exports = {
         'dotgothic16': ['DotGothic16'],
         'bungee-shade': ['Bungee Shade'],
       },
+      colors: {
+        primary: 'rgb(var(--colors-primary))',
+        background: 'rgb(var(--colors-background))',
+        container: 'rgb(var(--colors-container))',
+        'on-background': 'rgb(var(--colors-on-background))',
+        'on-container': 'rgb(var(--colors-on-container))',
+      },
     },
   },
   plugins: [],
