e5794e9f1e87c1e8888a9c0a8309fc66c5f4856b
Minakami <necromancer@minakami.land>
2024-09-10 02:09:27 +0800

    feat(app): apply `MMM DD YYYY` format for post dates

--- app/src/utils/date.ts
+++ app/src/utils/date.ts
@@ -1,7 +1,9 @@
-export const formatDateToMonthYearString = (date: Date): string => {
+export const formatDate = (date: Date, includeDay: boolean = true): string => {
   const options: Intl.DateTimeFormatOptions = {
     year: 'numeric',
     month: 'short',
+    ...(includeDay && { day: 'numeric' }),
   }
+
   return new Intl.DateTimeFormat('en-US', options).format(date)
 }
--- studio/schemas/post.ts
+++ studio/schemas/post.ts
@@ -36,7 +36,7 @@ export default defineType({
     defineField({
       name: 'publishedAt',
       title: 'Published at',
-      type: 'datetime',
+      type: 'date',
     }),
     defineField({
       name: 'isPostListVisible',
